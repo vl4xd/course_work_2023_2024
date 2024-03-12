1f09a26fdbbfd2dc7713eec59f3036272cb32f30
vl4xd <vlaxddd@gmail.com>
2024-03-13 01:16:13 +0300

    qwerty

--- createTable.php
+++ createTable.php
@@ -1,10 +1,12 @@
 <?php
     require_once 'header.php';
 
-    if (!$loggedin ||!isset($_GET['team_id'])){
+    if (!$loggedin || !isset($_GET['team_id'])){
         redirectToTime('./index.php', 0);
         exit;
     }
+
+    //проверка на возможность редактирования таблицы table_id пользвоателем 
 
     $error = $table_name = "";
     $columnCounter = 0;
@@ -13,27 +15,13 @@
     $team_public = false;
     $jsonTypes = json_encode($GLOBALS['types']); // передача массива с типами в js
 
-echo<<<_END
-    <script>
-        function addColumn(childrenCounter) {
-            $('.columns').append("<div data-role='fieldcontain'>").enhanceWithin(); 
-            $('.columns').append("<input type='text' data-inline='true'>").enhanceWithin();
-            $('.columns').append("</div>").enhanceWithin();
-        }
-        
-        function deleteColumn(column_id){
-
-        }
-    </script>
-_END;
-
     if(isset($_POST['table_name'])){
         $table_name = sanitizeString($_POST['table_name']);
         $table_info = sanitizeString($_POST['table_info']);
     }
 
 echo <<<_END
-            <form method='post' action='createTable.php'>$error
+            <form method='post' action='./createTable.php?team_id=$team_id'>$error
             <div data-role='fieldcontain'>
                 <label></label>
                 Пожайлуйста введите данные: Название таблицы, Описание
@@ -49,16 +37,7 @@ echo <<<_END
                 <textarea cols="40" rows="8" name="table_info" id="table_info"></textarea>
             </div>
             <div class="columns">
-                
             </div>
-            <script>
-                var childrenCounter = $('.columns').children().length;
-                if (childrenCounter == 0){
-                    for (var i = 0; i < 3; i++){
-                        addColumn(childrenCounter);
-                    }
-                }
-            </script>
             <div data-role='fieldcontain'>
                 <label></label>
                 <input type='button' value='Добавить' onClick="addColumn()">
@@ -68,6 +47,40 @@ echo <<<_END
                 <input data-transition='slide' type='submit' value='Создать'>
             </div>
             </form>
+_END;
+
+echo<<<_END
+    <script>
+        var columnsCounter = $('.columns').children().length;
+
+        function deleteColumn(column_id){
+            $('[class^="' + column_id + '"]').remove();
+        }
+
+        function addColumn() {
+            var n = 'column_id_' + columnsCounter;
+
+            var newDiv = $('<div class="' + n + '" data-role="fieldcontain"></div>');
+            var newInput = $('<input id="' + n + '" name="' + n + '" type="text" data-inline="true">');
+            var newButton = $('<a data-inline="true" class="ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all"></a>');
+            newButton.attr('onClick', 'deleteColumn(\'' + n + '\')');
+            newDiv.append(newInput);
+            newDiv.append(newButton);
+            $('.columns').append(newDiv).enhanceWithin();
+
+            newButton.on('click', function() {
+                deleteColumn(n);
+            });
+
+            columnsCounter++;
+        }
+
+        if (columnsCounter == 0){
+            for (var i = 0; i < 3; i++){
+                addColumn();
+            }
+        }
+    </script>
 _END;
 
     require_once 'footer.php';
--- test.js
+++ test.js
@@ -8,3 +8,21 @@ $('.columns').append("<select name="select-choice-mini" id="select-choice-mini" 
             $('.columns').append("</select>");
             $('.columns').append("<a class="ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all" data-inline='true'>No text</a>");
             $('.columns').append("</div>");
+
+
+$('.columns').append("<div data-role='fieldcontain'></div>")
+.attr({
+    'class': 'column_id_' + childrenCounter,
+})
+.enhanceWithin(); 
+$('.column_id_' + childrenCounter).append("<input type='text' data-inline='true'>")
+.attr({
+    'id': 'column_id_' + childrenCounter,
+    'name': 'column_id_' + childrenCounter,
+})
+.enhanceWithin();
+$('.column_id_' + childrenCounter).append("<a data-role='button' data-inline='true' data-icon='delete'>No text</a>")
+.attr({
+    'onClick': 'deleteColumn(column_id_' + childrenCounter + ')',
+})
+.enhanceWithin();
