1cdf33746b20f417c2b6c213ac2f013bef2d8555
vl4xd <vlaxddd@gmail.com>
2024-03-17 13:59:56 +0300

    111

--- tableView.php
+++ tableView.php
@@ -20,7 +20,7 @@ echo<<<_END
 _END;
 
 echo <<< _TABLES
-    <ul id='teams' data-role="listview" data-filter="true" data-filter-placeholder="Имя таблицы" data-inset="true">
+    <ul id='teams' data-role="listview" data-filter="true" data-filter-placeholder="Данные записи" data-inset="true">
 _TABLES;
 
     $result_notes = queryMysql("SELECT notes.id as note_id, items.id as item_id, types.name as type FROM items
@@ -32,36 +32,40 @@ _TABLES;
 
     $current_note_id = $next_note_id = 0;
     $value = $hrefNote = "";
+
+    if ($num_notes != 0){
+        for ($i = 0; $i < $num_notes; $i++){        
+            $row_notes = $result_notes->fetch_array(MYSQLI_BOTH);
     
-    for ($i = 0; $i < $num_notes; $i++){        
-        $row_notes = $result_notes->fetch_array(MYSQLI_BOTH);
-
-        if ($i == 0) {
-            $current_note_id = $row_notes['note_id'];
+            if ($i == 0) {
+                $current_note_id = $row_notes['note_id'];
+            }
+    
+            $result_item = queryMysql('SELECT * FROM ' . $row_notes['type'] . ' WHERE item_id =' . $row_notes['item_id']);
+            $row_item = $result_item->fetch_array(MYSQLI_BOTH);
+    
+    
+            $next_note_id = $row_notes['note_id'];
+            if ($current_note_id == $next_note_id){
+                $value .= $row_item['data'] . " ";
+                
+            }
+            else{
+                $hrefNote = "./noteView.php?note_id=" . $current_note_id;
+                echo "<li><a data-transition='slide' href='$hrefNote'>$value</a></li>";
+                $current_note_id = $next_note_id;
+                $value = "";
+                $value .= $row_item['data'] . " ";
+            }
         }
 
-        $result_item = queryMysql('SELECT * FROM ' . $row_notes['type'] . ' WHERE item_id =' . $row_notes['item_id']);
-        $row_item = $result_item->fetch_array(MYSQLI_BOTH);
-
-
-        $next_note_id = $row_notes['note_id'];
-        if ($current_note_id == $next_note_id){
-            $value .= $row_item['data'] . " ";
-            
-        }
-        else{
-            $hrefNote = "./noteView.php?note_id=" . $current_note_id;
-            echo "<li><a data-transition='slide' href='$hrefNote'>$value</a></li>";
-            $current_note_id = $next_note_id;
-            $value = "";
-            $value .= $row_item['data'] . " ";
-        }
+        $hrefNote = "./noteView.php?note_id=" . $current_note_id;
+        echo "<li><a data-transition='slide' href='$hrefNote'>$value</a></li>";
+        
+        $result_notes->close();
+        $result_item->close();
     }
 
-    echo "<li><a data-transition='slide' href='$hrefNote'>$value</a></li>";
-    $result_notes->close();
-    $result_item->close();
-    
 echo <<<_TABLES
         </ul>
 _TABLES;
